9d9a154ac47a6034fc0aa0d0285d6e0838a7d083
MTarique51 <mohdtarique7223@gmail.com>
2026-06-13 09:41:48 -1200

    .

--- 72_Edit_Distance.java
+++ 72_Edit_Distance.java
@@ -12,13 +12,13 @@ class Solution
             f[i][0] = i;
             for (int j = 1; j <= n; ++j) 
             {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) 
+                if (word1.charAt(i-1) == word2.charAt(j-1)) 
                 {
                     f[i][j] = f[i-1][j-1];
                 } 
                 else 
                 {
-                    f[i][j] = Math.min(f[i-1][j], Math.min(f[i][j - 1], f[i - 1][j - 1])) + 1;
+                    f[i][j] = Math.min(f[i-1][j], Math.min(f[i][j-1], f[i-1][j-1])) + 1;
                 }
             }
         }
